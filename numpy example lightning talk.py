9eaa2b553c22e57c7fb0f61f1efb44cfd62cc7d8
SajanDayal17 <143046236+SajanDayal17@users.noreply.github.com>
2023-10-23 14:53:47 -0500

    Update numpy example lightning talk.py

--- numpy example lightning talk.py
+++ numpy example lightning talk.py
@@ -2,7 +2,10 @@
 """
 Created on Wed Oct 11 14:32:26 2023
 
-@author: sajan
+@author: Sajan Dayal
+ME369P
+Lightning Talk
+Dream Team
 """
 
 import numpy as np
@@ -15,16 +18,37 @@ arr4 = np.array([True, True, False])
 
 #Truth Testing
 istrue=np.all(arr3)
-print(istrue)
+print('Is it true: ', istrue)
 
 #Logical Operations
 logical=np.logical_and(arr3, arr4)
-print(logical)
+print('Logical Operations: ', logical)
 
 #Comparison
 comparison=np.greater(arr1, arr2)
-print(comparison)
+print('Is is greater: ', comparison)
 
 #ArrayContents
 contents=np.isfinite(arr1)
-print(contents)
+print('Is it finite: ',contents)
+isnotanumber=np.isnan(arr1)
+print('Is not a number: ', isnotanumber)
+
+#Descriptive Statistics
+mean_value = np.mean(arr1)
+print('Mean: ', mean_value)
+median_value = np.median(arr1)
+print('Median: ',median_value)
+std_deviation = np.std(arr1)
+print('Standard Deviation: ',std_deviation)
+
+#Percentiles and Quartiles
+percentile_25 = np.percentile(arr1, 25)
+percentile_75 = np.percentile(arr1, 75)
+print('25th Percentile: ', percentile_25)
+print('75th Percentile: ', percentile_75)
+
+#Check for outliers
+z_scores = (arr1 - np.mean(arr1)) / np.std(arr1)
+outliers = np.abs(z_scores) > 2
+print('Is an outlier: ',outliers)
